ccbeb401f4dafedf4b2162b2dadea5787c9f0c5e
Kiera Wheatley <kiera.wheatley@activision.com>
2022-07-19 11:44:14 -0400

    final solution

--- Ch2Challenge2/Ch2Challenge2/Ch2Challenge2.cpp
+++ Ch2Challenge2/Ch2Challenge2/Ch2Challenge2.cpp
@@ -2,20 +2,50 @@
 //
 
 #include <iostream>
-#include <math.h>
+#include <cmath>
+
+using namespace std;
+
+double a, cubedValue;
+
+void GetUserInput();
+void DisplayErrorMessage();
+void DisplayValueCubed();
+double CalculateCubedValue(double b);
+
+void GetUserInput()
+{
+    cout << "Please enter a number: " << endl;
+
+    cin >> a;
+
+    cubedValue = CalculateCubedValue(a);
+}
+
+void DisplayErrorMessage()
+{
+    cout << "Invalid input, please try running the program again." << endl;
+}
+
+void DisplayCubedValue()
+{
+    cout << "Cubed Value = " << cubedValue << endl;
+}
+
+double CalculateCubedValue(double b)
+{
+    return pow(b, 3);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    GetUserInput();
+
+    if (cin.fail())
+    {
+        DisplayErrorMessage();
+        return 0;
+    }
+
+    DisplayCubedValue();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
